b57ab12d4a979bcca3f9e1255d9bc5dfeb222438
JCIII <jcostello711@gmail.com>
2022-04-30 15:01:13 -0400

    added route for posting a new User

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const {
     getAllUsers,
+    createUser,
 } = require('../../controllers/user-controller');
 
 // /api/users
 router 
     .route('/')
-    .get(getAllUsers);
+    .get(getAllUsers)
+    .post(createUser);
 
 module.exports = router;
