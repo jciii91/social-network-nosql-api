ebfcc5ebca05cb3e820d4c81a31f9097e79161a2
JCIII <jcostello711@gmail.com>
2022-04-30 14:52:35 -0400

    user controller added. added getAllUsers

---
+++ controllers/user-controller.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+}
