fd1200615fda87f5eec6b3597b4dc2d46df50e29
JCIII <jcostello711@gmail.com>
2022-04-30 14:48:15 -0400

    User model added

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        }
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
