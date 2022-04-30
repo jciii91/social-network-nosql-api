321997e6034e3896503b5381a7fa5c307b6ae556
JCIII <jcostello711@gmail.com>
2022-04-30 15:00:18 -0400

    added createUser function to user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -11,6 +11,13 @@ const userController = {
             res.sendStatus(400);
         });
     },
+
+    //createUser
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
 }
 
 module.exports = userController;
